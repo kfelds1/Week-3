32e3f6fc3993af55f9ff59908185dce4188a3476
kfelds1 <112633146+kfelds1@users.noreply.github.com>
2022-10-20 18:56:33 -0400

    Add files via upload

---
+++ 10.18.22/3-1-Student-Resources/07-Evr_HobbyBook-Dictionaries/Unsolved/HobbyBook_Unsolved.py
@@ -0,0 +1,17 @@
+# @TODO: Your code here
+pet_dict = {
+    "Pet_Name":"Mae",
+    "Pet_Age": 11,
+    "Hobbies":["barking", "licking", "eating"],
+    "Wake_up":["7AM", "12:30PM"]
+
+}
+
+print(f"My pet's name is {pet_dict['Pet_Name']}. {pet_dict['Pet_Name']}'s age is {pet_dict['Pet_Age']}.")
+
+print(f"{pet_dict['Pet_Name']} has {len(pet_dict['Hobbies'])} hobbies.")
+
+print(f"{pet_dict['Pet_Name']}'s favorite hobby is {pet_dict['Hobbies'][0]}.")
+
+print(f"{pet_dict['Pet_Name']} wakes up at {pet_dict['Wake_up'][0]} on Saturdays.")
+
